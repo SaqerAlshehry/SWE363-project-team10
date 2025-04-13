a85183a6af1052642ac0b8ead6c2c412ae5ac8b2
SAQER-ALSHEHRI <riche.homme.10@gmail.com>
2025-04-13 06:25:59 +0300

    Implement Profile Page UI with reusable components

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../styles/Avatar.css"
+
+function Avatar({ name, image, onEdit }) {
+  return (
+    <div className="avatar-container">
+      <div className="avatar-header">
+        <span className="avatar-name">{name}</span>
+        <span className="edit-icon" onClick={onEdit}>✏️</span>
+      </div>
+      <div className="avatar-circle">
+        <img src={image} alt={name} className="avatar-image" />
+      </div>
+    </div>
+  );
+}
+
+export default Avatar;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import TextField from '../components/TextField';
+import Avatar from '../components/Avatar';
+import { FiUser, FiMail, FiPhone, FiLock, FiHome } from 'react-icons/fi';
+import '../styles/Profile.css'
+
+function Profile() {
+  const [name, setName] = useState('Ahmed s');
+  const [email, setEmail] = useState('s200000000@kfupm.edu.sa');
+  const [phone, setPhone] = useState('+966');
+  const [password, setPassword] = useState('');
+  const [building, setBuilding] = useState('862');
+  const [room, setRoom] = useState('111');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Profile saved!');
+  };
+
+  return (
+    <div className="profile-page">
+        <div className="profile-card">
+      <Avatar name={name} image="/assets/kfupm.jpg" onEdit={() => alert('Edit clicked')} />
+
+      <form onSubmit={handleSubmit} className="form-grid">
+        <TextField label="Name" value={name} onChange={(e) => setName(e.target.value)} icon={<FiUser />} />
+        <TextField label="Phone Number" value={phone} onChange={(e) => setPhone(e.target.value)} icon={<FiPhone />} />
+        <TextField label="Email" value={email} onChange={(e) => setEmail(e.target.value)} icon={<FiMail />} />
+        <TextField label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} icon={<FiLock />} />
+        <TextField label="Building" value={building} onChange={(e) => setBuilding(e.target.value)} icon={<FiHome />} />
+        <TextField label="Room" value={room} onChange={(e) => setRoom(e.target.value)} icon={<FiHome />} />
+
+        <div className="button-row">
+          <button type="submit" className="save-button">Save</button>
+          <button type="button" className="change-password-button" onClick={() => alert('Change Password')}>Change Password</button>
+        </div>
+      </form>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
