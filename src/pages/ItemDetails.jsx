ec22293ca58cee51e45f2c48c5c71c2c969f8f94
SaudMaashi <saudm45q@gmail.com>
2025-04-18 19:23:13 +0300

    Removing remaining parts of comments feature of the product details page

--- src/pages/ItemDetails.jsx
+++ src/pages/ItemDetails.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
 import "../styles/ItemDetails.css";
-import { useState } from "react";
-import TextField from "../components/TextField";
 
 function ItemDetails() {
     const location = useLocation();
@@ -10,22 +8,10 @@ function ItemDetails() {
     const data = params.get("data");
     const decodedData = decodeURIComponent(data);
     const itemObjectProperties = JSON.parse(decodedData);
-
     const itemName = itemObjectProperties[0];
     const itemDescription = itemObjectProperties[1];
     const itemDonation = itemObjectProperties[2];
     const itemImage = itemObjectProperties[3];
-
-    const comments = [
-        "Thanks, Saud!",
-        "Excellent quality.",
-        "Very satisfied with the purchase.",
-        "Would recommend to others.",
-        "I love this!",
-        "Very useful.",
-    ];
-
-    const [userComment, setUserComment] = useState("");
     return (
         <div className="item-page">
             <div className="item-card">
